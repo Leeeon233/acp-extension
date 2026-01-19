f16a63da9766e2e25d9ecaf9f833aebe2ee13f17
leeeon233 <leeeon233@gmail.com>
2026-01-19 12:09:53 +0800

    feat: rare limits

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export const EXT_METHOD_NAME = {
-    usage_update: 'lody:session_usage_update',
-    title_generate: 'lody:session_title_generate',
+    usage_update: 'acp_ext:session_usage_update',
+    title_generate: 'acp_ext:session_title_generate',
+    rate_limits: 'acp_ext:session_rate_limits'
 } as const;
 
 export type EXT_METHOD_NAME_TYPE =
@@ -25,4 +26,25 @@ export type SessionUsageUpdate = {
         reasoningOutputTokens?: number,
     },
     modelUsage?: { [key: string]: ModelUsage }
+}
+
+export type AccountPlanType = "free" | "plus" | "pro" | "team" | "business" | "enterprise" | "edu" | "unknown"
+
+export type RateLimitSnapshot = {
+    primary?: RateLimitWindow;
+    secondary?: RateLimitWindow;
+    credits?: CreditsSnapshot;
+    planType?: AccountPlanType;
+}
+
+export type RateLimitWindow = {
+    usedPercent: number;
+    windowMinutes: number;
+    resetsAt?: number
+}
+
+export type CreditsSnapshot = {
+    hasCredits: boolean;
+    unlimited: boolean;
+    balance?: string;
 }
